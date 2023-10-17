feece0e1d0ceb32c05d219b5baf2457307453592
jussara2 <144683878+jussara2@users.noreply.github.com>
2023-10-17 11:00:17 -0300

    questao_11
    atividade

---
+++ 11q.py
@@ -0,0 +1,9 @@
+n1= int(input('digite um numero inteiro'))
+n2= int(input('digite um numero inteiro'))
+n3= float(input('digite um numero real'))
+
+a= (n1*2)*(n2/2)
+b= (n1*3) + n3
+c=n3**3
+
+print(a,b,c)
